f7e3ee9ee4453b938acd82e7e4ca7a33d2af87ab
bifot <bifot@bifot.ru>
2018-05-30 17:14:25 +0300

    added promisify for base64 method

--- base64-img.js
+++ base64-img.js
@@ -40,15 +40,17 @@ function img(data) {
  * @example
  * base64Img.base64('path/demo.png', function(err, data) {})
  */
-exports.base64 = function(filename, callback) {
-  if (!callback) callback = util.noop;
-
-  fs.readFile(filename, function(err, data) {
-    if (err) return callback(err);
-
-    callback(null, base64(filename, data));
-  });
-};
+exports.base64 = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(base64(filename, data))
+      }
+    })
+  })
+}
 
 /**
  * @description
